72c9f759b1b5ab9f6c637414d07e179f2b3c75cf
yuemillo <106904623+yuemillo@users.noreply.github.com>
2023-12-03 16:32:36 +0800

    Revert "Paul"

--- js/helper/tokenHandler.js
+++
@@ -1,9 +0,0 @@
-
-export default function checkToken(){
-    return localStorage.getItem('server-token') ?? false;
-}
-
-export function setToken(token){
-    localStorage.setItem('server-token',token)
-}
-
--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-
-//搜尋功能
-import searchPost from './js/searchPost';
-
-searchPost();
-
+console.log("Hello world!");

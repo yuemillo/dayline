bf3524b51877af559127baa9d8516660a7ec6eb8
unknown <poola12320@gmail.com>
2023-12-13 00:11:32 +0800

    Updates

---
+++ assets/about-8a8dc6dc.js
@@ -0,0 +1,2 @@
+import{S as t}from"./main-b65e9ec0.js";async function n(){await t.fire({icon:"info",iconColor:"hsl(0,75%,43%)",title:"注意!",allowOutsideClick:"false",text:`刪除貼文將會終止日更記錄，確定要刪除?
+當日日更，需再重新完成`,buttonsStyling:!1,customClass:{confirmButton:"btn px-5 btn-danger me-6",denyButton:"btn px-5 btn-outline-secondary"},showDenyButton:!0,confirmButtonText:"刪除",denyButtonText:"取消"})}n();
